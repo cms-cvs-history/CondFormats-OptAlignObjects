1c1781ad17007de31f7c1993a4b6901e014afd0e
Mike Case <sha1-1d692184473970509f0e165e4d62c8c025ba163f@cern.ch>
2006-03-09 15:14:08 +0000

    add handle for XXXXMeasurements add persistency for XXXXMeasurements

--- test/testOptAlignWriter.cc
+++ test/testOptAlignWriter.cc
@@ -16,14 +16,9 @@
 #include <string>
 #include <iostream>
 int main(){
-//   pool::POOLContext::loadComponent( "SEAL/Services/MessageService" );
-//   pool::POOLContext::setMessageVerbosityLevel( seal::Msg::Info );
-//   cond::DBWriter w(std::string("sqlite_file:test.db"));
-//   w.startTransaction();
-  
+
   OpticalAlignments* oa(new OpticalAlignments);
   OpticalAlignInfo oainfo;
-  //  OpticalAlignParam x, y, z, xAng, yAng, zAng;
   OpticalAlignParam lValue, aValue;
 
   lValue.value_ = 1.0123;
@@ -33,25 +28,6 @@ int main(){
   aValue.value_ = 1.0123;
   aValue.error_ = 0.00123;
   aValue.qual_ = oa_unknown;
-
-//   x.value_ = 1.0123;
-//   y.value_ = 1.0123;
-//   z.value_ = 1.0123;
-//   x.error_ = 0.0123;
-//   y.error_ = 0.0123;
-//   z.error_ = 0.0123;
-//   x.qual_ = oa_unknown;
-//   y.qual_ = oa_unknown;
-//   z.qual_ = oa_unknown;
-//   xAng.value_ = 1.0123;
-//   yAng.value_ = 1.0123;
-//   zAng.value_ = 1.0123;
-//   xAng.error_ = 0.00123;
-//   yAng.error_ = 0.00123;
-//   zAng.error_ = 0.00123;
-//   xAng.qual_=oa_unknown;
-//   yAng.qual_=oa_unknown;
-//   zAng.qual_=oa_unknown;
 
   oainfo.x_ = lValue;
   oainfo.y_ = lValue;
@@ -68,59 +44,58 @@ int main(){
   oa->opticalAlignments_.push_back(oainfo);
   std::cout << "done with push_back of oainfo to oa" << std::endl;
 
-	      pool::POOLContext::loadComponent( "SEAL/Services/MessageService" );
-	      pool::POOLContext::setMessageVerbosityLevel( seal::Msg::Info );
-	      cond::ServiceLoader* loader=new cond::ServiceLoader;
-	      ::putenv("CORAL_AUTH_USER=me");
-	      ::putenv("CORAL_AUTH_PASSWORD=mypass");
-	      loader->loadAuthenticationService( cond::Env );
-	      loader->loadMessageService( cond::Error );
-	      cond::DBSession* session=new cond::DBSession("sqlite_file:test.db");
-	      session->setCatalog("file:PoolFileCatalog.xml");
-	      session->connect(cond::ReadWriteCreate);
+  pool::POOLContext::loadComponent( "SEAL/Services/MessageService" );
+  pool::POOLContext::setMessageVerbosityLevel( seal::Msg::Info );
+  cond::ServiceLoader* loader=new cond::ServiceLoader;
+  ::putenv("CORAL_AUTH_USER=me");
+  ::putenv("CORAL_AUTH_PASSWORD=mypass");
+  loader->loadAuthenticationService( cond::Env );
+  loader->loadMessageService( cond::Error );
+  cond::DBSession* session=new cond::DBSession("sqlite_file:test.db");
+  session->setCatalog("file:PoolFileCatalog.xml");
+  session->connect(cond::ReadWriteCreate);
 
-	      cond::DBWriter pw(*session, "OpticalAlignments");
-	      cond::DBWriter iovw(*session, "IOV");
-	      cond::IOV* initiov=new cond::IOV;
-	      session->startUpdateTransaction();
-	      //	      std::string tok=pw.markWrite<CSCZSensors>(csczs);
-	      std::string tok1 = pw.markWrite<OpticalAlignments>(oa);
+  cond::DBWriter pw(*session, "OpticalAlignments");
+  cond::DBWriter iovw(*session, "IOV");
+  cond::IOV* initiov=new cond::IOV;
+  session->startUpdateTransaction();
+  //	      std::string tok=pw.markWrite<CSCZSensors>(csczs);
+  std::string tok1 = pw.markWrite<OpticalAlignments>(oa);
+
+  lValue.value_ = 1.0234;
+  lValue.error_ = 0.0234;
+  lValue.qual_ = oa_unknown;
+
+  aValue.value_ = 1.0234;
+  aValue.error_ = 0.00234;
+  aValue.qual_ = oa_unknown;
+
+  oainfo.x_ = lValue;
+  oainfo.y_ = lValue;
+  oainfo.z_ = lValue;
+  oainfo.angx_ = aValue;
+  oainfo.angy_ = aValue;
+  oainfo.angz_ = aValue;
+  oainfo.objectType_ = "plate";
+  oainfo.objectID_ = 1;
 
   OpticalAlignments* oatoo=new OpticalAlignments;
   oatoo->opticalAlignments_.push_back(oainfo);
+  oatoo->opticalAlignments_.push_back(oainfo);
+  oatoo->opticalAlignments_.push_back(oainfo);
   std::string tok2=pw.markWrite<OpticalAlignments>(oatoo);
 
-	      initiov->iov.insert(std::make_pair(1,tok1));
-	      initiov->iov.insert(std::make_pair(3,tok2));
-	      std::string iovtok = iovw.markWrite<cond::IOV>(initiov);
-	      session->commit();
-	      session->disconnect();
-	      cond::MetaData metadata_svc("sqlite_file:test.db", *loader);
-	      metadata_svc.connect();
-	      metadata_svc.addMapping("OpticalAlignmentsTry3", iovtok);
-	      metadata_svc.disconnect();
-	      std::cout << "Wrote OpticalAlignments to sqlite_file:test.db";
-	      std::cout << " with token " << iovtok << " as OpticalAlignmentsTry3 "  << std::endl;
-	      delete session;
+  initiov->iov.insert(std::make_pair(1,tok1));
+  initiov->iov.insert(std::make_pair(3,tok2));
+  std::string iovtok = iovw.markWrite<cond::IOV>(initiov);
+  session->commit();
+  session->disconnect();
+  cond::MetaData metadata_svc("sqlite_file:test.db", *loader);
+  metadata_svc.connect();
+  metadata_svc.addMapping("OpticalAlignmentsTry3", iovtok);
+  metadata_svc.disconnect();
+  std::cout << "Wrote OpticalAlignments to sqlite_file:test.db";
+  std::cout << " with token " << iovtok << " as OpticalAlignmentsTry3 "  << std::endl;
+  delete session;
 
-//   std::string tok1=w.write<OpticalAlignments>(oa,"OpticalAlignments");
-//   std::cout << "done writing" << std::endl;
-
-//   cond::IOV* initiov=new cond::IOV;
-//   initiov->iov.insert(std::make_pair(1,tok1));
-//   initiov->iov.insert(std::make_pair(2,tok2));
-//   std::string iovtok1=w.write<cond::IOV>(initiov, "IOV");
-
-//   w.commitTransaction();
-
-//   w.startTransaction();
-//   cond::IOV* bestiov=new cond::IOV;
-//   bestiov->iov.insert(std::make_pair(10,tok1));
-//   bestiov->iov.insert(std::make_pair(20,tok2));
-//   std::string iovtok2=w.write<cond::IOV>(bestiov, "IOV");
-//   w.commitTransaction();
-
-//   cond::MetaData metadata_svc("sqlite_file:test.db");
-//   metadata_svc.addMapping("OpticalAlignmentsTry3", iovtok1);
-//   metadata_svc.addMapping("OpticalAlignmentsTry4", iovtok2);
 }
